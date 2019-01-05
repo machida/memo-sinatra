b1488b00fe1f986648b50c1c401de0af9cd9d82d
teppei machida <machida@fjord.jp>
2019-01-06 01:55:07 +0900

    ファイルを生成できるようにした

--- memo.rb
+++ memo.rb
@@ -1,5 +1,27 @@
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader"
 
-get '/' do
-  'Hello world!'
+get "/" do
+  erb :index
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/create" do
+  @title = params[:title]
+  @body = params[:body]
+  File.open("data/#{@title}.txt", "w"){|f|
+    f.puts @title
+    f.puts @body
+  }
+  redirect "/#{@title}"
+end
+
+get '/:title' do |title|
+  f = File.open("data/#{title}.txt")
+  s = f.read
+  f.close
+  p s
 end
