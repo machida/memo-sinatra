dfc3e888bc926b5b660644b46a06e9e8ce24d11d
teppei machida <machida@fjord.jp>
2019-01-07 01:59:14 +0900

    memoクラスを作って整理

--- memo.rb
+++ memo.rb
@@ -1,6 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 
+class Memo
+  def initialize(data)
+    memo_contents = []
+    File.open(data, mode = "rt") do |f|
+      memo_contents = f.readlines
+    end
+    @title = memo_contents[0]
+    @body = memo_contents[1]
+  end
+
+  def title
+    @title
+  end
+
+  def body
+    @body
+  end
+end
+
 get "/" do
   @memos = Dir.glob("data/*.txt")
   erb :index
@@ -13,16 +32,14 @@ end
 post "/create" do
   @title = params[:title]
   @body = params[:body]
-  File.open("data/#{@title}.txt", "w"){|f|
+  File.open("data/#{@title}.txt", "w") do |f|
     f.puts @title
     f.puts @body
-  }
+  end
   redirect "/#{@title}"
 end
 
 get '/:title' do |title|
-  f = File.open("data/#{title}.txt")
-  s = f.read
-  f.close
-  p s
+  @memo = "data/#{title}.txt"
+  erb :show
 end
