e67c331be9ff8bd7f4b2822b1da7fce2113c51a7
teppei machida <machida@fjord.jp>
2019-01-07 23:56:55 +0900

    slim化 editを実装

--- memo.rb
+++ memo.rb
@@ -1,14 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "slim"
 
 class Memo
   def initialize(data)
     memo_contents = []
-    File.open(data, mode = "rt") do |f|
+    File.open(data, "rt") do |f|
       memo_contents = f.readlines
     end
     @title = memo_contents[0]
-    @body = memo_contents[1]
+    @body = memo_contents[1..-1].join
   end
 
   def title
@@ -18,15 +19,21 @@ class Memo
   def body
     @body
   end
+
+  def url
+    "/#{@title}"
+  end
 end
 
 get "/" do
+  @title = "メモ"
   @memos = Dir.glob("data/*.txt")
-  erb :index
+  slim :index
 end
 
 get "/new" do
-  erb :new
+  @title = "新規メモ"
+  slim :new
 end
 
 post "/create" do
@@ -40,6 +47,25 @@ post "/create" do
 end
 
 get '/:title' do |title|
-  @memo = "data/#{title}.txt"
-  erb :show
+  @title = title
+  memo = "data/#{title}.txt"
+  @memo = Memo.new(memo)
+  slim :show
+end
+
+get '/:title/edit' do |title|
+  @title = "#{title}内容変更"
+  memo = "data/#{title}.txt"
+  @memo = Memo.new(memo)
+  slim :edit
+end
+
+post "/:title/update" do
+  @title = params[:title]
+  @body = params[:body]
+  File.open("data/#{@title}.txt", "r+") do |f|
+    f.puts @title
+    f.puts @body
+  end
+  redirect "/#{@title}"
 end
